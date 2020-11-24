95a90c9abd3846e93ea08addf9ed219cf37a5780
CarlosPerez93 <carlos.perez@udla.edu.co>
2020-11-24 14:18:16 -0500

    feat: update home

---
+++ src/Views/Public/Home/CardProxEvents/cardProxEvents.jsx
@@ -0,0 +1,14 @@
+import { Card, Col } from 'antd'
+import React from 'react'
+
+export default function cardProxEvents() {
+    return (
+        <Card 
+            className="card-main"
+            hoverable
+
+        >
+            
+        </Card>
+    )
+}
--- src/Views/Public/Home/Home.js
+++ src/Views/Public/Home/Home.js
@@ -1,7 +1,7 @@
-import React, { useEffect} from "react";
+import React, { useEffect } from "react";
 // import api from "../../../common/api/api";
 // import token from "../../../localstorage/token";
-import { Carousel, Tabs, Col, Row, Card } from "antd";
+import { Carousel, Col, Row, Card } from "antd";
 // import { Link } from "react-router-dom";
 import "./Home.css";
 import img1 from "../../../Assests/Img/1.jpg";
@@ -11,19 +11,9 @@ import img4 from "../../../Assests/Img/4.png";
 import img5 from "../../../Assests/Img/5.png";
 import img6 from "../../../Assests/Img/6.png";
 import img7 from "../../../Assests/Img/7.png";
-import img8 from "../../../Assests/Img//Decoracion/deco1.jpg";
-import img9 from "../../../Assests/Img/Decoracion/deco2.jpg";
-import img10 from "../../../Assests/Img/Decoracion/deco3.jpg";
-import img11 from "../../../Assests/Img/Lugar/Lugar1.jpg";
-import img12 from "../../../Assests/Img/Lugar/Lugar2.jpg";
-import img13 from "../../../Assests/Img/Lugar/Lugar3.jpg";
-import img14 from "../../../Assests/Img/Buffet/Buffet1.jpg";
-// import img15 from "../../../Assests/Img/Buffet/Buffet2.jpg";
-// import img16 from "../../../Assests/Img/Buffet/Buffet3.jpg";
-import img17 from "../../../Assests/Img/Fotografia/Fotog1.jpg";
-import img18 from "../../../Assests/Img/Fotografia/Fotog2.jpg";
-import img19 from "../../../Assests/Img/Fotografia/Fotog3.jpg";
-import img20 from "../../../Assests/Img/Animacion/Animacio1.png";
+import Search from "antd/lib/input/Search";
+
+
 
 
 const Home = () => {
@@ -33,18 +23,16 @@ const Home = () => {
     //  const result = await api.get("user/profile", { id: token.decodeJWT().id });
     //  setUser(result.data);
   };
-  function callback(key) {
-    console.log(key);
-  }
+
   useEffect(() => {
     dataApi();
   });
-  const { TabPane } = Tabs;
+
 
   return (
-    <Col lg={{span:18, offset:3}} className="mainHome">
-    
-      <Carousel autoplay>
+    <Col lg={{ span: 18, offset: 3 }} xs={{ span: 18, offset: 3 }} className="mainHome">
+
+      <Carousel >
         <div className="cont">
           <img
             className="imgs"
@@ -73,137 +61,49 @@ const Home = () => {
           <img className="imgs" src={img7} alt=" " />
         </div>
       </Carousel>
+      <Search className='search' />
 
-      <div className="containerAvatars">
-        <Tabs onChange={callback} className="tab">
-          <TabPane key="1" tab="decoracion" className="tab1">
-            <Carousel autoplay className="carrousel">
-              <div className="cont">
-                <img className="imgs" src={img8} alt=" " />
-              </div>
-
-              <div className="cont">
-                <img className="imgs" src={img9} alt=" " />
-              </div>
-              <div className="cont">
-                <img className="imgs" src={img10} alt=" " />
-              </div>
-            </Carousel>
-          </TabPane>
-
-          <TabPane key="2" tab="Lugares" className="tab1">
-            <Carousel autoplay className="carrousel">
-              <div className="cont">
-                <img className="imgs" src={img11} alt=" " />
-              </div>
-
-              <div className="cont">
-                <img className="imgs" src={img12} alt=" " />
-              </div>
-              <div className="cont">
-                <img className="imgs" src={img13} alt=" " />
-              </div>
-            </Carousel>
-          </TabPane>
-          <TabPane key="3" tab="Buffet" className="tab1">
-            <Carousel autoplay className="carrousel">
-              <div className="cont">
-                <img className="imgs" src={img14} alt=" " />
-              </div>
-
-              <div className="cont">
-                <img className="imgs" src={img2} alt=" " />
-              </div>
-              <div className="cont">
-                <img className="imgs" src={img3} alt=" " />
-              </div>
-            </Carousel>
-          </TabPane>
-          <TabPane key="4" tab="Sonido" className="tab1">
-            <Carousel autoplay className="carrousel">
-              <div className="cont">
-                <img className="imgs" src={img1} alt=" " />
-              </div>
-
-              <div className="cont">
-                <img className="imgs" src={img2} alt=" " />
-              </div>
-              <div className="cont">
-                <img className="imgs" src={img3} alt=" " />
-              </div>
-            </Carousel>
-          </TabPane>
-          <TabPane key="5" tab="Fotografia" className="tab1">
-            <Carousel autoplay className="carrousel">
-              <div className="cont">
-                <img className="imgs" src={img17} alt=" " />
-              </div>
-
-              <div className="cont">
-                <img className="imgs" src={img18} alt=" " />
-              </div>
-              <div className="cont">
-                <img className="imgs" src={img19} alt=" " />
-              </div>
-            </Carousel>
-          </TabPane>
-          <TabPane key="6" tab="Animación" className="tab1">
-            <Carousel autoplay className="carrousel">
-              <div className="cont">
-                <img className="imgs" src={img20} alt=" " />
-              </div>
-
-              <div className="cont">
-                <img className="imgs" src={img2} alt=" " />
-              </div>
-              <div className="cont">
-                <img className="imgs" src={img3} alt=" " />
-              </div>
-            </Carousel>
-          </TabPane>
-        </Tabs>
-      </div>
-      <h1 className="h11">Nuestros Servicios</h1>
-      <br />
-      <br />
+      <Col lg={{ span: 6, offset: 2 }} xs={{ span: 6, offset: 2 }}>
+            
+      </Col>
 
       <Row className="about">
         <Col lg={{ span: 6, offset: 1 }}>
-         <Card style={{borderRadius: 6}} className="card-about">
-         <h2>Misión</h2>
-          <p>
-            Re-BesT nace con la misión de satisfacer las necesidades de nuestros
-            clientes a la hora de buscar un servicio personalizado de excelente
-            calidad para la organización de todo tipo de eventos, teniendo en
-            cuenta cada detalle para que sea inolvidable.
-          </p>
-         </Card>
-        </Col>
-        <Col lg={{ span: 6, offset: 1 }}>
-          <Card style={{borderRadius: 6}} className="card-about">
-          <h2>Visión</h2>
-          <p>
-            Nuestra empresa Re-BesT, busca liderar el mercado nacional de
-            empresas organizadoras de eventos, innovando nuestros servicios con
-            el propósito de satisfacer las exigencias de nuestros clientes.
+          <Card style={{ borderRadius: 6 }} className="card-about">
+            <h2>Misión</h2>
+            <p>
+              Re-BesT nace con la misión de satisfacer las necesidades de nuestros
+              clientes a la hora de buscar un servicio personalizado de excelente
+              calidad para la organización de todo tipo de eventos, teniendo en
+              cuenta cada detalle para que sea inolvidable.
           </p>
           </Card>
         </Col>
         <Col lg={{ span: 6, offset: 1 }}>
-          <Card style={{borderRadius: 6}} className="card-about">
-          <h2>Valores</h2>
-          <p>
-            Cumplimiento, respeto, calidad, creatividad, confianza, serán
-            nuestros principios para tener éxito, atender los eventos con
-            puntualidad, estar al tanto de los cambios para implementarlos en
-            los servicios que ofrecemos, tratar a todos por igual, que nuestros
-            eventos sean recordados por nuestros clientes y nos recomienden.
+          <Card style={{ borderRadius: 6 }} className="card-about">
+            <h2>Visión</h2>
+            <p>
+              Nuestra empresa Re-BesT, busca liderar el mercado nacional de
+              empresas organizadoras de eventos, innovando nuestros servicios con
+              el propósito de satisfacer las exigencias de nuestros clientes.
+          </p>
+          </Card>
+        </Col>
+        <Col lg={{ span: 6, offset: 1 }}>
+          <Card style={{ borderRadius: 6 }} className="card-about">
+            <h2>Valores</h2>
+            <p>
+              Cumplimiento, respeto, calidad, creatividad, confianza, serán
+              nuestros principios para tener éxito, atender los eventos con
+              puntualidad, estar al tanto de los cambios para implementarlos en
+              los servicios que ofrecemos, tratar a todos por igual, que nuestros
+              eventos sean recordados por nuestros clientes y nos recomienden.
           </p>
           </Card>
         </Col>
       </Row>
 
-      <h1>Quienes Somos</h1>
+
     </Col>
   );
 };
